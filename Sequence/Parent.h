2c3d4ab5cf49de1a48eea66707c2117f01ae7892
SeeYouCowboi <teacatjazz@gmail.com>
2023-11-20 00:10:31 +0800

    添加项目文件。

---
+++ Sequence/Parent.h
@@ -0,0 +1,45 @@
+﻿#ifndef INCLUDED_SEQUENCE_PARENT_H
+#define INCLUDED_SEQUENCE_PARENT_H
+
+namespace Sequence{
+class Child;
+
+class Parent{
+public:
+	enum NextSequence{
+		NEXT_TITLE,
+		NEXT_GAME,
+		NEXT_GAME_OVER,
+		NEXT_ENDING,
+
+		NEXT_NONE,
+	};
+	enum Mode{
+		MODE_4P,
+		MODE_5P,
+		MODE_6P,
+		MODE_NONE,
+	};
+	void update();
+	void moveTo( NextSequence );
+	Mode getMode() const;
+	void setMode( Mode );
+
+	static void create();
+	static void destroy();
+	static Parent* instance();
+private:
+	Parent();
+	~Parent();
+
+	NextSequence mNextSequence;
+	Mode mMode;
+
+	Child* mChild;
+
+	static Parent* mInstance;
+};
+
+} //namespace Sequence
+
+#endif
